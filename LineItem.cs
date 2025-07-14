37dd0847d2e3778838e8e8d5c14ffee512590ab9
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-07-14 20:23:27 +0530

    Create LineItem.cs

---
+++ LineItem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartAssignment
+{
+    internal class LineItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+
+        public LineItem(int  id, int quantity, Product product)
+        {
+            Id = id;
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public double CalculateLineItemCost()
+        {
+            return Quantity * Product.CalculateDiscountedPrice();
+        }
+    }
+}
