105dbeda1227b310d8177ce2d14273b5e33dabbc
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-07-14 20:24:08 +0530

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartAssignment
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double DiscountPercent { get; set; }
+
+        public Product(int id, string name, double price, double discountPercent)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            DiscountPercent = discountPercent;
+        }
+
+        public double CalculateDiscountedPrice()
+        {
+            return Price - (DiscountPercent * Price / 100);
+        }
+    }
+}
